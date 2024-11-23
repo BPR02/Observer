bd00c7fa226654d2505a1e28af535d9baf15d19d
BPR02 <dev@bpr02.com>
2024-11-22 19:39:14 -0800

    worldgen

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -6,7 +6,12 @@ from beet import ProjectCache, run_beet
 from lectern import Document
 from pytest_insta import SnapshotFixture
 
-EXAMPLES = [f for f in os.listdir("examples") if not (f.startswith("nosnap_") or f.startswith("."))]
+EXAMPLES = [
+    f
+    for f in os.listdir("examples")
+    if not (f.startswith("nosnap_") or f.startswith("."))
+]
+
 
 @pytest.mark.parametrize("directory", EXAMPLES)
 def test_build(snapshot: SnapshotFixture, directory: str, tmp_path: Path):
@@ -14,4 +19,6 @@ def test_build(snapshot: SnapshotFixture, directory: str, tmp_path: Path):
         directory=f"examples/{directory}",
         cache=ProjectCache(tmp_path / ".beet_cache", tmp_path / "generated"),
     ) as ctx:
-        assert snapshot("pack.md") == ctx.inject(Document)
+        expected: Document = snapshot("pack.md")
+        actual = ctx.inject(Document)
+        assert actual == expected
